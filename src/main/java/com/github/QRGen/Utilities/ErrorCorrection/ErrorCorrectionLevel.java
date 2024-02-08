913d9ac6e3ee4eb2653de67c00087f2ddba82dc5
chayan199916 <swapnomoy199916@gmail.com>
2024-02-09 02:30:36 +0530

    feat : error correction version logic added;

---
+++ src/main/java/com/github/QRGen/Utilities/ErrorCorrection/ErrorCorrectionLevel.java
@@ -0,0 +1,18 @@
+package com.github.QRGen.Utilities.ErrorCorrection;
+
+public class ErrorCorrectionLevel {
+    private char level;
+
+    public ErrorCorrectionLevel(char level) {
+        this.level = level;
+    }
+
+    public char getLevel() {
+        return level;
+    }
+
+    public void setLevel(char level) {
+        this.level = level;
+    }
+
+}
