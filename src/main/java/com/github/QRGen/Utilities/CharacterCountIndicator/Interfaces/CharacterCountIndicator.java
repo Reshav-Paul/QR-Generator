0dac552df50ce320c7878a7f4cea9725e9454c32
chayan199916 <swapnomoy199916@gmail.com>
2024-02-23 02:04:15 +0530

    feat : enum added for versions;

--- src/main/java/com/github/QRGen/Utilities/CharacterCountIndicator/Interfaces/CharacterCountIndicator.java
+++ src/main/java/com/github/QRGen/Utilities/CharacterCountIndicator/Interfaces/CharacterCountIndicator.java
@@ -5,6 +5,20 @@ import com.github.QRGen.Utilities.QRCodeVersion.QRCodeVersionException;
 import com.github.QRGen.Utilities.QRCodeVersion.Interfaces.QRCodeVersion;
 
 public interface CharacterCountIndicator {
+    public enum QRVersion {
+        M1(0), M2(1), M3(2), M4(3), OneToNine(4), TenToTwentySix(5), TwentySevenToForty(6);
+
+        private final int value;
+
+        private QRVersion(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
     public String getCCI() throws CharacterCountIndicatorException, QRCodeVersionException;
 
     public int getCharacterCount();
